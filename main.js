f4440e63677b9cad989341b4e78c26fa38cae82a
EtaoinWu <etaoin.wu@gmail.com>
2021-10-27 16:59:55 +0800

    initial

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 350,
+    height: 680,
+    titleBarStyle: 'hidden',
+    transparent: true,
+    resizable: true
+  })
+
+  win.loadFile('index.html')
+  win.setAlwaysOnTop(true, "pop-up-menu")
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
